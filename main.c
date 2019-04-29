c069202697ded0b052cf24d412301c383c990b88
Lewis Rosenbauer <lewis.rosenbauer@uon.edu.au>
2019-04-29 05:43:24 +0000

    Submitt

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-//Caesar cipher
+//Rotation cipher
 
+char sample1 [101]; //store text in sample1
 
 int main(){
     
-    printf("enter message to encrypt");
+    char*rotationencrypt (char *sample1, int key);  
+    
+    int count, key; //key count initalise for shifting 
+    char letter;
+    printf("message to encrypt");
+    scanf(  );
+    
+    for //need a for loop
+    
+    if //if statement 
+    
+    
+    
+    
     
     
     return 0; 
